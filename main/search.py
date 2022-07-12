9b1d20bc436fc81f75922ec06ba90dd7827494ff
mojja-im <82865792+ImRieul@users.noreply.github.com>
2022-07-12 11:45:41 +0900

    Temporary search

--- main/search.py
+++ main/search.py
@@ -2,41 +2,77 @@ from main import *
 
 
 def slice_address(address):
-    cut_comma_dot = address.split(',')[0].split('.')[0]  # 상세주소 자르기
-    # cut_detail = cut_comma_dot[:cut_comma_dot.find('층') - 1] if cut_comma_dot.find('층') != -1 \
-    #     else cut_comma_dot  # 상세주소 자르기, ","로 구분이 안 되어있을 경우
-    detail_address = [',', '.', '지하', '층', '***', '상가동', '(']
-    detail_address_set = []
+    detail_address = [',', '.', '지하', '층', '호', '상가동', '상가', '(', '***']
+    index_correction = {'층': -1, '호': -3}
+    add = address
     for detail in detail_address:
-        if address.find(detail) != -1:
-            pass
-            # address[:address.find(detail) if ]
+        if add.find(detail) != -1:
+            add = add[:add.find(detail) if detail not in index_correction.keys() else add.find(detail)
+                                                                                      + index_correction[detail]]
 
-    cut_detail = cut_comma_dot[:cut_comma_dot.find('지하') if cut_comma_dot.find('지하') != -1 else cut_comma_dot.find('층') -1 if cut_comma_dot.find('층') != -1 else len(cut_comma_dot)]
-    return cut_detail.split('(')[0]
+    return add
 
 
 if __name__ == '__main__':
     file_name = 'sample.xlsx'
+    count_error = 0
+
     with excel.Excel(file_name, index_row=1) as excel:
-        road_col = []
-        count_error = 0
+        address_col = []  # 지번 주소
+        road_address_col = []  # 도로명 주소
+        address_3depth_name_col = []  # 법정동
+        b_code_col = []  # 법정 코드
+        address_3depth_h_name_col = []  # 행정동
+        h_code_col = []  # 행정 코드
+
         for i in excel['가맹점주소']:
             query = slice_address(i)
             try:
+                # 검색
                 result = address.Address(query=query)
-                road_col_save = f"{result.road_address['address_name']} " \
-                                f"{result.address['region_3depth_name']}" \
-                                f" {result.road_address['building_name']}" if result.road_address[
-                                                                                  'building_name'] != '' else ''
 
-                road_col.append(road_col_save)
+                # 지번 주소
+                address_col_save = result.address['address_name']
+                address_col.append(address_col_save)
+
+                # 도로명 주소
+                road_address_col_save = f"{result.road_address['address_name']} " \
+                                        f"({result.address['region_3depth_name']}"
+                road_address_col_save += f", {result.road_address['building_name']})" if result.road_address[
+                                                                                             'building_name'] != '' else ')'
+                road_address_col.append(road_address_col_save)
+
+                # 행정동
+                address_3depth_h_name_col.append(result.address['region_3depth_h_name'])
+                h_code_col.append(result.address['h_code'])
+
+                # 법정동
+                address_3depth_name_col.append(result.address['region_3depth_name'])
+                b_code_col.append(result.address['b_code'])
+
             except TypeError as te:
                 print(f'error address : {i}, query value : {query}')
                 count_error += 1
-                road_col.append('')
+                address_col.append('')
+                road_address_col.append('')
+                address_3depth_h_name_col.append('')
+                h_code_col.append('')
+                address_3depth_name_col.append('')
+                b_code_col.append('')
             except Exception as e:
+                count_error += 1
                 print(e)
 
-        print(road_col)
+        excel['지번주소'] = address_col
+        excel['도로명주소'] = road_address_col
+        excel['행정동'] = address_3depth_h_name_col
+        excel['행정동코드'] = h_code_col
+        excel['법정동'] = address_3depth_name_col
+        excel['법정동코드'] = b_code_col
+
+        print(road_address_col)
         print(f'에러가 난 주소는 {count_error}개 입니다.')
+
+    # print(
+    #     slice_address('대전 서구 문정로2번길 51,1층 108호 (탄방동,시티빌3차)')
+    # )
