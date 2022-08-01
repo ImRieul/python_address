a51366c04042ea596546d490b23b758b9b59360e
mojja-im <82865792+ImRieul@users.noreply.github.com>
2022-08-01 17:51:26 +0900

    update search class Search

--- main/search.py
+++ main/search.py
@@ -1,22 +1,21 @@
-from typing import Any
+from main.excel import *
+from main.address.address import *
 
-from address.address import *
-from excel import *
-
+import pandas as pd
 
 # TODO 주소 못찾으면 콘솔 입력
 #  에러 수정
 #  주소 잘 입력했는지 검증
 #  주소 DB 생성
-#  slice_address 리펙토링
+#  slice_address 리팩토링
+
 
 class Search:
-    def __init__(self, box: list):
-        self.box = {i: [] for i in box}
+    def __init__(self, put_columns: list, save_columns: list):
+        self.box = pd.DataFrame(columns=put_columns)
 
     def cutting_data(self, input_data: list):
-        for index, value in enumerate(self.box.keys()):
-            self.box[value].append(input_data[index])
+        self.box.loc[len(self.box.index)] = input_data
 
     # def search(self, address: Address, count: int = 0)
     #     if int != 0:
@@ -24,7 +23,6 @@ class Search:
     #         count += 1
     #
     #     query = input()
-
 
 
 def slice_address(address_name: str):
@@ -39,82 +37,53 @@ def slice_address(address_name: str):
     return add
 
 
-def search_yourself(fail_address: Address, count: int = 3):
-    print('주소가 검색되지 않았습니다. 정확한 주소를 입력해주세요')
-    print('검색한 문자열:', fail_address.query)
+def search_yourself(fail_query: str, fail_address: Address, count: int = 1):
+    if fail_address.is_search_type(AddressSearchType.REGION_ADDR, AddressSearchType.ROAD_ADDR):
+        return fail_address
+
+    print(f"주소가 검색되지 않았습니다. 정확한 주소를 입력해주세요. ({count} 번째)")
+    print('검색한 문자열:', fail_query)
     print('넘기고 싶다면 pass를 입력하세요')
     query = input()
 
-    if query == 'pass' or 0 > count or count > 3:
+    if query == 'pass' or 0 > count or count >= 3:
         return Address(query='pass')
 
     search_address = Address(query=query, analyze_type=fail_address.analyze_type)
     return search_address \
         if search_address.is_search_type(AddressSearchType.REGION_ADDR, AddressSearchType.ROAD_ADDR) \
-        else search_yourself(search_address, count - 1)
+        else search_yourself(query, search_address, count + 1)
 
 
 if __name__ == '__main__':
     file_name = 'sample.xlsx'
     # file_name = '84. 옥외광고물 허가 신고 현황_4번.xlsx'
     count_error = 0
+    excel_columns = {
+        AddressEnum.ADDRESS_NAME: '지번주소',
+        RoadAddressEnum.ADDRESS_NAME: '도로명주소',
+        AddressEnum.REGION_3DEPTH_NAME: '법정동',
+        AddressEnum.REGION_3DEPTH_H_NAME: '행정동',
+    }
 
-    with Excel(file_name, index_row=2) as excel:
+    with Excel(file_name, index_row=1) as excel:
+        search = Search(list(excel_columns.keys()), list(excel_columns.values()))
 
-        search = Search([
-            AddressEnum.ADDRESS_NAME.value,  # 지번 주소
-            RoadAddressEnum.ADDRESS_NAME.value,  # 도로명 주소
-            AddressEnum.REGION_3DEPTH_NAME.value,  # 법정동
-            AddressEnum.REGION_3DEPTH_H_NAME.value,  # 행정동
-        ])
-
-        for i in excel['가맹점주소']:
+        for i in excel['주소']:
             query = slice_address(i)
-            address = Address(query=query)
+            address = search_yourself(i, Address(query=query))
 
-            search.cutting_data([address.get_address_name(),
-                                 address.get_road_address_fullname(),
-                                 address.get_address_name(AddressEnum.REGION_3DEPTH_NAME)])
-                                 address.get_address_name(AddressEnum.REGION_3DEPTH_H_NAME)])
+            search.cutting_data([
+                address.get_address_name(),
+                address.get_road_address_fullname(),
+                address.get_address_name(AddressEnum.REGION_3DEPTH_NAME),
+                address.get_address_name(AddressEnum.REGION_3DEPTH_H_NAME),
+            ])
 
-        #     try:
-        #         # 검색
-        #         result = address.Address(query=query)
-        #
-        #         address_col.append(result.get_address_name())
-        #         road_address_col.append(result.get_road_address_fullname())
-        #         address_3depth_h_name_col.append(result.get_address_name(AddressEnum.REGION_3DEPTH_H_NAME))
-        #         h_code_col.append(result.get_address_name(AddressEnum.H_CODE))
-        #         address_3depth_name_col.append(result.get_address_name(AddressEnum.REGION_3DEPTH_NAME))
-        #         b_code_col.append(result.get_address_name(AddressEnum.B_CODE))
-        #
-        #     # 지번 주소는 있으나 도로명 주소가 없는 경우가 있음, 그럴 땐 지번 주소의 배열만 늘어남.
-        #     # 지번 주소의 배열과 엑셀의 행의 수가 맞지 않으면 에러.
-        #     except TypeError as te:
-        #         print(f'error address : {i}, query value : {query}')
-        #         count_error += 1
-        #         address_col.append('')
-        #         road_address_col.append('')
-        #         address_3depth_h_name_col.append('')
-        #         h_code_col.append('')
-        #         address_3depth_name_col.append('')
-        #         b_code_col.append('')
-        #     except Exception as e:
-        #         count_error += 1
-        #         print(e)
-        #
-        # # 같은 컬럼 이름이 있으면 .1부터 시작해서 숫자가 더해짐.
-        # # save
-        # excel['지번주소'] = address_col
-        # excel['도로명주소'] = road_address_col
-        # excel['행정동'] = address_3depth_h_name_col
-        # excel['행정동코드'] = h_code_col
-        # excel['법정동'] = address_3depth_name_col
-        # excel['법정동코드'] = b_code_col
-        #
-        # print(road_address_col)
-        # print(f'에러가 난 주소는 {count_error}개 입니다.')
+        # 같은 컬럼 이름이 있으면 .1부터 시작해서 숫자가 더해짐.
+        # save
+        for key, value in excel_columns.items():
+            excel[value] = search.box[key].values
 
-    # print(
-    #     slice_address('대전 서구 문정로2번길 51,1층 108호 (탄방동,시티빌3차)')
-    # )
+        print(search.box[RoadAddressEnum.ADDRESS_NAME])
+        print(f'에러가 난 주소는 {count_error}개 입니다.')
--- test/test_search.py
+++ test/test_search.py
@@ -1,28 +1,14 @@
 import unittest
-from unittest.mock import patch
 from main.search import *
 
 
 # search_yourself
-class SearchYourself(unittest.TestCase):
-    def test_exist(self):
-        query = '대전 서구 둔산로 100'
-        with patch('builtins.input', side_effect=[query]):
-            search_address = search_yourself(address.Address())
-
-        self.assertEqual(query, search_address.get_road_address_name())
-
-    def test_not_exist(self):
-        query = '커피한잔할래요'
-        with patch('builtins.input', side_effect=[query]):
-            search_address = search_yourself(address.Address())
-        self.assertEqual('', search_address.get_road_address_name())
-
-    def test_param_type_error(self):
-        query = '대전 서구 둔산로 100'
-        with patch('builtins.input', side_effect=[query]):
-            with self.assertRaises(AttributeError):
-                search_yourself('hello')
+class CuttingData(unittest.TestCase):
+    def test_ok(self):
+        search = Search(['number', 'alphabet'])
+        input_data = [1, 'a']
+        search.cutting_data(input_data)
+        self.assertEqual(list(search.box.loc[0]), input_data)
 
 
 if __name__ == '__main__':
