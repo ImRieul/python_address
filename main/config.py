2f045377e4983761119ecb5690d99daba97929ae
mojja-im <82865792+ImRieul@users.noreply.github.com>
2022-09-25 22:18:51 +0900

    update config.py with OS

--- main/config.py
+++ main/config.py
@@ -1,6 +1,40 @@
 import os
+import platform
 
-ROOT_PATH = '/'.join(
-    os.path.dirname(__file__) \
-        .split('/')[:len(os.path.dirname(__file__).split('/')) - 1]
-)
+
+def path_encoding(path: str) -> str:
+    """
+    윈도우와 맥의 디렉토리 구분이 다르기에 맞춰주는 함수
+    window = \, mac = /
+    """
+    if platform.system() == "Windows":
+        return path.replace("/", "\\")
+    elif platform.system() == "Darwin":
+        return path.replace("\\", "/")
+    return path
+
+
+# ROOT_PATH = '/'.join(
+#     os.path.dirname(__file__) \
+#         .split('/')[:len(os.path.dirname(__file__).split('/')) - 1]
+# )
+
+
+def root_path() -> str:
+    if platform.system() == "Windows":
+        return '\\'.join(
+            os.path.dirname(__file__) \
+            .split('\\')[:len(os.path.dirname(__file__).split('\\')) - 1]
+        )
+    elif platform.system() == "Darwin":
+        return '/'.join(
+            os.path.dirname(__file__) \
+                .split('/')[:len(os.path.dirname(__file__).split('/')) - 1]
+        )
+
+
+if __name__ == '__main__':
+    print(1, root_path())
+    print(2, __file__)
+    print(3, os.path.dirname(__file__))
+    print(4, path_encoding('C:/programming/find_address/main/config.py'))
